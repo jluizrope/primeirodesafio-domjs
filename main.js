8ea3fe6a01e0f943cbedf610a3781fbc980dbea7
vai na web <vainawebcpx@gmail.com>
2019-10-28 12:42:32 -0300

    iniciei o primeiro desafio do Dom-Js, usei um projeto pra fazer este desafio. Falta títulos e textos

---
+++ main.js
@@ -0,0 +1,52 @@
+let imagesList = [
+	{
+		img: "https://cdn.pixabay.com/photo/2017/01/23/09/21/learn-2001838_960_720.jpg",
+		title: 'A CHALLENGE',
+		text: 'This is the first challenge I and my classmates of the second stage of VNW Course were submitted. I took the advantage of a project I had done to do this challenge.'
+
+	},
+	{ 
+		img: 'https://cdn.pixabay.com/photo/2016/03/27/18/54/technology-1283624_960_720.jpg',
+		title: 'AN INITIAL EXAMPLE'
+
+	},
+	{
+		img: 'https://as1.ftcdn.net/jpg/01/87/07/58/500_F_187075892_2YDNhbbk2W7Mzd9iP8XFiYXHaQcnwgvo.jpg',
+		title: 'AN AIM TO ACHIVE'
+	},
+	{
+		img: 'https://cdn.pixabay.com/photo/2016/09/26/09/08/binary-1695475_960_720.jpg',
+		title: 'GLOBE IN PROGRAM'
+	}
+]
+
+let counter=0
+
+let left = document.getElementById('left')
+let img=document.getElementById('img')
+let right=document.getElementById('right')
+
+left.addEventListener('click', function(){
+	console.log('click')
+	if (counter > 0) {
+		counter = counter - 1;
+		img.setAttribute('src', imagesList[counter].img);
+	}else {
+		counter = 3
+		img.setAttribute('src', imagesList[counter].img);
+	}
+})
+
+right.addEventListener('click', function(){
+	if (counter < 3) 
+	{
+		counter = counter + 1;
+		img.setAttribute('src', imagesList[counter].img);
+
+	}else {
+
+		counter = 0
+		img.setAttribute('src', imagesList[counter].img);
+	}
+})
+
